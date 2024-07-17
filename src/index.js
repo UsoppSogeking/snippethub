d78a16751d0fbdc8534cb2d6fb15f9fe1fddc5fd
Kleber <lyuri1508@gmail.com>
2024-07-17 15:19:21 -0300

    Setimo commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const db = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const snippetsRoutes = require('./routes/snippetsRoutes');
 const commentsRoutes = require('./routes/commentsRoutes');
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
 app.use(express.json());
 
 db.authenticate()
@@ -20,7 +28,7 @@ app.use('/', userRoutes);
 app.use('/', snippetsRoutes);
 app.use('/', commentsRoutes);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
