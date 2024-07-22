c19ac34f81d62d841a00b94b37fa0dc9c0d17052
Kleber <lyuri1508@gmail.com>
2024-07-22 13:43:24 -0300

    atualizacao do middleware de snippet

--- src/routes/snippetsRoutes.js
+++ src/routes/snippetsRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { createSnippet, getSnippets, updateSnippet, deleteSnippet } = require('../controllers/snippetController');
 const authenticateToken = require('../middlewares/authenticateToken');
+const validateSnippet = require('../middlewares/validateSnippet');
 
 const router = express.Router();
 
-router.post('/snippets', authenticateToken, createSnippet);
+router.post('/snippets', authenticateToken, validateSnippet, createSnippet);
 router.get('/getsnippets', authenticateToken, getSnippets);
-router.put('/updatesnippet/:id', authenticateToken, updateSnippet);
+router.put('/updatesnippet/:id', authenticateToken, validateSnippet, updateSnippet);
 router.delete('/deletesnippet/:id', authenticateToken, deleteSnippet);
 
 module.exports = router;
