fdc3e007c8b44799a7b9683953dafeb1972aca29
Kleber <lyuri1508@gmail.com>
2024-07-24 18:25:13 -0300

    att do model de snippet

--- src/models/Snippet.js
+++ src/models/Snippet.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 const User = require('./User');
+const Comment = require('./Comment');
 
 const Snippet = sequelize.define('Snippet', {
     title: {
@@ -24,5 +25,7 @@ const Snippet = sequelize.define('Snippet', {
 Snippet.belongsTo(User, {
     foreignKey: 'user_id',
 });
+
+Snippet.hasMany(Comment, { foreignKey: 'snippet_id', onDelete: 'CASCADE' });
 
 module.exports = Snippet;
