5166d98931a5a65818cebadd589d74f075a7327b
Kleber <lyuri1508@gmail.com>
2024-07-24 14:03:37 -0300

    image upload

--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -2,11 +2,12 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/profile_picture/');
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/profile_picture');
     },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
     }
 });
 
